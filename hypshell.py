40a266ee8a6f886c3884f160f04238604650773f
Hyper <90523348+Hyper100@users.noreply.github.com>
2023-12-12 01:14:11 +0200

    Added license information

--- hypshell.py
+++ hypshell.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
+
+"""
+HypShell - a simple reverse shell handler and automatic stabilizer
+Author: Ofir Havkin - aka Hyper
+
+HypShell is a simple reverse shell handler and automatic stabilizer written in Python 3.
+It can be used instead of netcat to handle reverse shells and stabilize them automatically.
+
+Copyright (C) 2023 Ofir Havkin - aka Hyper
+
+HypShell is a free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+HypShell is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import argparse
 import socket
 import os
